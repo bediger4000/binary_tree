0d3c7e34f79e74299217fe2aa779a11e7f241649
Bruce Ediger <bediger8@gmail.com>
2020-05-17 11:12:40 -0600

    Invert a binary tree

---
+++ invert.go
@@ -0,0 +1,45 @@
+package main
+
+/*
+Invert a binary tree.
+
+For example, given the following tree:
+
+    a
+   / \
+  b   c
+ / \  /
+d   e f
+should become:
+
+  a
+ / \
+ c  b
+ \  / \
+  f e  d
+
+*/
+
+import (
+	"binary_tree/tree"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	root := tree.RandomValueTree(100, 15, true)
+
+	if root != nil {
+		fmt.Printf("digraph g1 {\n")
+		fmt.Printf("subgraph cluster_0 {\n\tlabel=\"before\"\n")
+		tree.DrawPrefixed(os.Stdout, root, "a")
+		fmt.Printf("\n}\n")
+		root.Invert()
+		fmt.Printf("subgraph cluster_1 {\n\tlabel=\"after\"\n")
+		tree.DrawPrefixed(os.Stdout, root, "b")
+		fmt.Printf("\n}\n")
+		fmt.Printf("\n}\n")
+
+	}
+}
