2a0aa772d8171fd2667aac68fe3c4c935fe73978
Bruce Ediger <bediger8@gmail.com>
2020-05-18 09:16:43 -0600

    InorderTraverseVistor and AllorderTraverseVistor
    Somewhat generalized traverse functions. Carries a function or
    functions through the entire traverse. Seems a bit Java-boilerplate-ish.
    Seems like I'm getting away from simplicity with this.

--- tree/traverse.go
+++ tree/traverse.go
@@ -12,6 +12,41 @@ func PreorderTraverse(node *Node) {
 	PreorderTraverse(node.Right)
 }
 
+// InorderTraverseVistor does a traverse of a binary tree,
+// callling a function in-order at every node
+func InorderTraverseVisit(node *Node, fn VisitorFunc) {
+	if node == nil {
+		return
+	}
+	InorderTraverseVisit(node.Left, fn)
+	fn(node)
+	InorderTraverseVisit(node.Right, fn)
+}
+
+// AllorderTraverseVistor does a traverse of a binary tree,
+// callling a function pre-order, in-order and post-order at every node
+func AllorderTraverseVisit(node *Node, preorderfn, inorderfn, postorderfn VisitorFunc) {
+	if node == nil {
+		return
+	}
+	preorderfn(node)
+	AllorderTraverseVisit(node.Left, preorderfn, inorderfn, postorderfn)
+	inorderfn(node)
+	AllorderTraverseVisit(node.Right, preorderfn, inorderfn, postorderfn)
+	postorderfn(node)
+}
+
+// PreorderTraverseVistor does a traverse of a binary tree,
+// callling a function in-order at every node
+func PreorderTraverseVisit(node *Node, fn VisitorFunc) {
+	if node == nil {
+		return
+	}
+	fn(node)
+	InorderTraverseVisit(node.Left, fn)
+	InorderTraverseVisit(node.Right, fn)
+}
+
 // InorderTraverse prints nodes' values on stdout in order.
 func InorderTraverse(node *Node) {
 	if node == nil {
