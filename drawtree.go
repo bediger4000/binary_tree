41c9cdeffadf9c985be593fd1e2482425ff56f61
Bruce Ediger <bediger8@gmail.com>
2020-05-18 13:28:06 -0600

    Use tree.CreateNumeric

--- drawtree.go
+++ drawtree.go
@@ -2,23 +2,10 @@ package main
 
 import (
 	"binary_tree/tree"
-	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
-	var root *tree.Node
-
-	for _, str := range os.Args[1:] {
-		val, err := strconv.Atoi(str)
-
-		if err == nil {
-			root = tree.Insert(root, val)
-		} else {
-			fmt.Fprintf(os.Stderr, "Problem with %q: %s\n", str, err)
-		}
-	}
-
+	root := tree.CreateNumeric(os.Args[1:])
 	tree.Draw(root)
 }
