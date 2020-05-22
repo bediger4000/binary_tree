77c8f643ad705ab61796e7337334ae40297c20f1
Bruce Ediger <bediger8@gmail.com>
2020-05-22 13:43:26 -0600

    Use as much package tree as possible

--- minimal_ht_tree.go
+++ minimal_ht_tree.go
@@ -7,52 +7,33 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"math/rand"
 	"os"
 	"sort"
 	"strconv"
 	"time"
+
+	"binary_tree/tree"
 )
 
-type TreeNode struct {
-	data  int
-	left  *TreeNode
-	right *TreeNode
-}
-
-func insert(node *TreeNode, value int) *TreeNode {
-
-	if node == nil {
-		return &TreeNode{data: value}
-	}
-
-	n := &(node.left)
-	if value >= node.data {
-		n = &(node.right)
-	}
-	*n = insert(*n, value)
-	return node
-}
-
-func minHeightInsert(sortedArray []int) (root *TreeNode) {
+func minHeightInsert(sortedArray []int) (root *tree.Node) {
 	sz := len(sortedArray)
 	switch sz {
 	case 1:
-		root = &TreeNode{data: sortedArray[0]}
+		root = &tree.Node{Data: sortedArray[0]}
 	case 2:
 		// Arrays of size two can end up in 2 arrangements:
 		if rand.Intn(2) == 0 {
-			root = &TreeNode{data: sortedArray[1]}
-			root.left = &TreeNode{data: sortedArray[0]}
+			root = &tree.Node{Data: sortedArray[1]}
+			root.Left = &tree.Node{Data: sortedArray[0]}
 		} else {
-			root = &TreeNode{data: sortedArray[0]}
-			root.right = &TreeNode{data: sortedArray[1]}
+			root = &tree.Node{Data: sortedArray[0]}
+			root.Right = &tree.Node{Data: sortedArray[1]}
 		}
 	case 3:
-		root = &TreeNode{data: sortedArray[1]}
-		root.left = &TreeNode{data: sortedArray[0]}
-		root.right = &TreeNode{data: sortedArray[2]}
+		root = &tree.Node{Data: sortedArray[1]}
+		root.Left = &tree.Node{Data: sortedArray[0]}
+		root.Right = &tree.Node{Data: sortedArray[2]}
 	default:
 		middle := sz / 2
 		// You've got a choice of "middle" for an even
@@ -60,39 +41,12 @@ func minHeightInsert(sortedArray []int) (root *TreeNode) {
 		if (middle % 2) == 0 {
 			middle -= rand.Intn(2)
 		}
-		root = &TreeNode{data: sortedArray[middle]}
-		root.left = minHeightInsert(sortedArray[0:middle])
-		root.right = minHeightInsert(sortedArray[middle+1:])
+		root = &tree.Node{Data: sortedArray[middle]}
+		root.Left = minHeightInsert(sortedArray[0:middle])
+		root.Right = minHeightInsert(sortedArray[middle+1:])
 
 	}
 	return root
-}
-
-func inorderTraverse(node *TreeNode) {
-	if node == nil {
-		return
-	}
-	inorderTraverse(node.left)
-	fmt.Printf("%d ", node.data)
-	inorderTraverse(node.right)
-}
-
-func drawTree(node *TreeNode) {
-	fmt.Printf("Node%p [label=\"%d\"];\n", node, node.data)
-	if node.left != nil {
-		drawTree(node.left)
-		fmt.Printf("Node%p -> Node%p;\n", node, node.left)
-	} else {
-		fmt.Printf("Node%pL [shape=\"point\"];\n", node)
-		fmt.Printf("Node%p -> Node%pL;\n", node, node)
-	}
-	if node.right != nil {
-		drawTree(node.right)
-		fmt.Printf("Node%p -> Node%p;\n", node, node.right)
-	} else {
-		fmt.Printf("Node%pR [shape=\"point\"];\n", node)
-		fmt.Printf("Node%p -> Node%pR;\n", node, node)
-	}
 }
 
 func main() {
@@ -116,46 +70,11 @@ func main() {
 
 	if root != nil {
 		fmt.Printf("/* In order traverse: ")
-		inorderTraverse(root)
+		tree.InorderTraverse(root)
 		fmt.Printf(" */\n")
-		fmt.Printf("digraph g {\n")
-		drawTree(root)
-		fmt.Printf("\n}\n")
-		if bstProperty(root) {
+		tree.Draw(root)
+		if tree.BstProperty(root) {
 			fmt.Printf("/* This is a binary search tree */\n")
 		}
 	}
-}
-
-// Return true if tree has "Binary seach tree"
-// property, false otherwise.
-func bstProperty(root *TreeNode) bool {
-	if !bst(root.left, math.MinInt32, root.data) {
-		return false
-	}
-	if !bst(root.right, root.data, math.MaxInt32) {
-		return false
-	}
-	return true
-}
-
-// function that actually checks BST property for
-// a given node somewhere in the tree.
-func bst(node *TreeNode, min int, max int) bool {
-
-	if node == nil {
-		return true
-	}
-
-	if !(node.data > min && node.data < max) {
-		return false
-	}
-	if !bst(node.left, min, node.data) {
-		return false
-	}
-	if !bst(node.right, node.data, max) {
-		return false
-	}
-
-	return true
 }
