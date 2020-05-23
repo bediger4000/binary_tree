07caa64f7faffdf18c4c2410b810d04539ede556
Bruce Ediger <bediger8@gmail.com>
2020-05-23 11:15:02 -0600

    Use package tree as much as possible

--- minimal_ht_tree2.go
+++ minimal_ht_tree2.go
@@ -6,75 +6,26 @@ package main
  */
 
 import (
+	"binary_tree/tree"
 	"fmt"
 	"math"
-	"math/rand"
 	"os"
+	"sort"
 	"strconv"
-	"time"
 )
 
-type TreeNode struct {
-	data  int
-	left  *TreeNode
-	right *TreeNode
-}
-
-func insert(node *TreeNode, value int) *TreeNode {
-
-	if node == nil {
-		return &TreeNode{data: value}
-	}
-
-	n := &(node.left)
-	if value >= node.data {
-		n = &(node.right)
-	}
-	*n = insert(*n, value)
-	return node
-}
-
-func minHeightInsert(sortedArray []int) (root *TreeNode) {
+func minHeightInsert(sortedArray []int) (root *tree.Node) {
 	root = nil
 	if sz := len(sortedArray); sz > 0 {
 		middle := sz / 2
-		root = &TreeNode{data: sortedArray[middle]}
-		root.left = minHeightInsert(sortedArray[0:middle])
-		root.right = minHeightInsert(sortedArray[middle+1:])
+		root = &tree.Node{Data: sortedArray[middle]}
+		root.Left = minHeightInsert(sortedArray[0:middle])
+		root.Right = minHeightInsert(sortedArray[middle+1:])
 	}
 	return root
 }
 
-func inorderTraverse(node *TreeNode) {
-	if node == nil {
-		return
-	}
-	inorderTraverse(node.left)
-	fmt.Printf("%d ", node.data)
-	inorderTraverse(node.right)
-}
-
-func drawTree(node *TreeNode) {
-	fmt.Printf("Node%p [label=\"%d\"];\n", node, node.data)
-	if node.left != nil {
-		drawTree(node.left)
-		fmt.Printf("Node%p -> Node%p;\n", node, node.left)
-	} else {
-		fmt.Printf("Node%pL [shape=\"point\"];\n", node)
-		fmt.Printf("Node%p -> Node%pL;\n", node, node)
-	}
-	if node.right != nil {
-		drawTree(node.right)
-		fmt.Printf("Node%p -> Node%p;\n", node, node.right)
-	} else {
-		fmt.Printf("Node%pR [shape=\"point\"];\n", node)
-		fmt.Printf("Node%p -> Node%pR;\n", node, node)
-	}
-}
-
 func main() {
-
-	rand.Seed(time.Now().UnixNano())
 
 	sortedArray := make([]int, 0)
 
@@ -85,52 +36,26 @@ func main() {
 		}
 	}
 
+	sort.Sort(sort.IntSlice(sortedArray))
+
 	fmt.Printf("/* Sorted array: %v */\n", sortedArray)
 
 	root := minHeightInsert(sortedArray)
 
 	if root != nil {
+		depth, _ := tree.FindDepth2(root, 0)
+		D := math.Log2(float64(len(sortedArray)) + 1.0)
+		f := float64(depth)
+		if f >= D && f <= D+1.0 {
+			fmt.Println("/* minimal height tree */")
+		}
+
 		fmt.Printf("/* In order traverse: ")
-		inorderTraverse(root)
+		tree.InorderTraverse(root)
 		fmt.Printf(" */\n")
-		fmt.Printf("digraph g {\n")
-		drawTree(root)
-		fmt.Printf("\n}\n")
-		if bstProperty(root) {
+		tree.Draw(root)
+		if tree.BstProperty(root) {
 			fmt.Printf("/* This is a binary search tree */\n")
 		}
 	}
-}
-
-// Return true if tree has "Binary seach tree"
-// property, false otherwise.
-func bstProperty(root *TreeNode) bool {
-	if !bst(root.left, math.MinInt32, root.data) {
-		return false
-	}
-	if !bst(root.right, root.data, math.MaxInt32) {
-		return false
-	}
-	return true
-}
-
-// function that actually checks BST property for
-// a given node somewhere in the tree.
-func bst(node *TreeNode, min int, max int) bool {
-
-	if node == nil {
-		return true
-	}
-
-	if !(node.data > min && node.data < max) {
-		return false
-	}
-	if !bst(node.left, min, node.data) {
-		return false
-	}
-	if !bst(node.right, node.data, max) {
-		return false
-	}
-
-	return true
 }
