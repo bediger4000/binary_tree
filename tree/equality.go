0750a787e30009297f189cd17417fac9ba8a09de
Bruce Ediger <bediger8@gmail.com>
2020-05-25 13:49:22 -0600

    Convert to using Node interface

--- tree/equality.go
+++ tree/equality.go
@@ -4,18 +4,38 @@ package tree
 // and each of their subtrees' nodes have the same values.
 // Trees that don't have the same structure (missing node(s),
 // or addtional node(s)) cause it to return false.
-func Equals(t1, t2 *NumericNode) bool {
-	if t1 == nil {
-		return t2 == nil
+func Equals(t1, t2 Node) bool {
+	// The both-nil and one-not-nil tests will answer
+	// incorrectly if t1 and t2 are not of the same type.
+	if t1.isNil() {
+		return t2.isNil()
 	}
-	if t2 == nil {
+	if t2.isNil() {
 		return false
 	}
-	if t1.Data != t2.Data {
+
+	switch ty1 := t1.(type) {
+	case *NumericNode:
+		switch ty2 := t2.(type) {
+		case *NumericNode:
+			if ty1.Data != ty2.Data {
+				return false
+			}
+		default:
+			return false
+		}
+	case *StringNode:
+		switch ty2 := t2.(type) {
+		case *StringNode:
+			if ty1.Data != ty2.Data {
+				return false
+			}
+		default:
+			return false
+		}
+	}
+	if !Equals(t1.leftChild(), t2.leftChild()) {
 		return false
 	}
-	if !Equals(t1.Left, t2.Left) {
-		return false
-	}
-	return Equals(t1.Right, t2.Right)
+	return Equals(t1.rightChild(), t2.rightChild())
 }
