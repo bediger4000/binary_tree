8b167d851f45c06de8587027ed3ace0ebe3ad714
Bruce Ediger <bediger8@gmail.com>
2020-06-24 21:19:43 -0600

    Add Daily Coding Problem statement

--- breadthfirst.go
+++ breadthfirst.go
@@ -1,7 +1,18 @@
 package main
 
 /*
- * Traverse a tree breadth-first, iteratively.
+ * Daily Coding Problem: Problem #107
+ * This problem was asked by Microsoft.
+ *
+ * Print the nodes in a binary tree level-wise. For example, the
+ * following should print 1, 2, 3, 4, 5.
+ *
+ *     1
+ *    / \
+ *   2   3
+ *  / \
+ * 4   5
+ *
  */
 
 import (
