a8bf6045cb997b4c3f0d6e92c956e94709c04757
Bruce Ediger <bediger8@gmail.com>
2020-10-30 08:45:19 -0600

    Remove wrong commentary

--- tree/balanced.go
+++ tree/balanced.go
@@ -6,8 +6,6 @@ import "fmt"
 // height-balanced binary tree can be defined as one in which the
 // heights of the two subtrees of any node never differ by more than
 // one.
-// This has to be part of package tree so it can use LeftChild(),
-// RightChild(), IsNil() methods.
 func Balanced(node Node) bool {
 	if node.IsNil() {
 		return true
