6b1fe0ef425b99edd38a31d34d24cf0f0a096200
Bruce Ediger <bediger8@gmail.com>
2020-12-29 12:36:35 -0700

    Fewer type assertions

--- breadthfirst.go
+++ breadthfirst.go
@@ -22,22 +22,19 @@ import (
 )
 
 func main() {
-	root := tree.CreateNumeric(os.Args[1:])
-	phrase := "has"
-	if !tree.BstProperty(root) {
-		phrase = "doesn't have"
-	}
-	fmt.Fprintf(os.Stderr, "tree %s binary search property\n", phrase)
+
+	root := tree.CreateNumericFromString(os.Args[1])
 
 	stack := &tree.Stack{}
 
 	stack.Push(root)
 
 	for !stack.Empty() {
-		n := stack.Dequeue()
-		fmt.Printf("%d ", n.(*tree.NumericNode).Data)
-		stack.Push(n.(*tree.NumericNode).Left)
-		stack.Push(n.(*tree.NumericNode).Right)
+		n := stack.Dequeue().(*tree.NumericNode)
+		fmt.Printf("%d ", n.Data)
+		stack.Push(n.Left)
+		stack.Push(n.Right)
 	}
+
 	fmt.Println()
 }
