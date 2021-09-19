6b503afa88503302d9934f7ac797108ac6099b44
Bruce Ediger <bediger8@gmail.com>
2021-09-19 11:25:41 -0600

    Use Node interface AllorderTraverseVisit
    An exercize to see how difficult using this more-or-less
    generic visitor function might be. Does not use globals,
    either.

---
+++ tree_depth_node.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"binary_tree/tree"
+	"fmt"
+	"os"
+)
+
+/*
+ * Daily Coding Problem: Problem #80
+ * Given the root of a binary tree, return a deepest node. For example,
+ * in the following tree, return d.
+ *
+ *     a
+ *    / \
+ *   b   c
+ *  /
+ * d
+ */
+
+func main() {
+	root := tree.CreateNumericFromString(os.Args[1])
+
+	if root != nil {
+		depth, node := findDepth(root)
+		fmt.Printf("Max depth %d, node value at depth %s\n", depth, node)
+	}
+}
+
+// findDepth using more-or-less generic tree.AllorderTraverseVisit,
+// and 3 closures using local variables as the visitor functions.
+// No globals so this is thread safe.
+func findDepth(root tree.Node) (maxDepth int, deepNode tree.Node) {
+
+	var depth int
+
+	preorderfun := func(node tree.Node) {
+		depth += 1
+	}
+	postorderfun := func(node tree.Node) {
+		depth -= 1
+	}
+	inorderfun := func(node tree.Node) {
+		if depth > maxDepth {
+			maxDepth = depth
+			deepNode = node
+		}
+	}
+
+	tree.AllorderTraverseVisit(root, preorderfun, inorderfun, postorderfun)
+
+	return
+}
