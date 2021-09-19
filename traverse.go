44523b7673018d5db8ea903ebacdfeff04dfe777
Bruce Ediger <bediger8@gmail.com>
2021-09-18 22:11:36 -0600

    Do in-order traverse using more general function

---
+++ traverse.go
@@ -0,0 +1,42 @@
+package main
+
+/*
+ * See if generalized AllorderTraverseVisit can do in-order traversal.
+ * It can.
+ */
+
+import (
+	"binary_tree/tree"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var root *tree.NumericNode
+	for i := range os.Args[1:] {
+		n, err := strconv.Atoi(os.Args[i+1])
+		if err == nil {
+			root = tree.Insert(root, n)
+		}
+	}
+	inordertraverse(root)
+	fmt.Println()
+}
+
+// inordertraverse sets up tree.AllorderTraverseVisit for in-order
+// traverse of a tree.
+func inordertraverse(root *tree.NumericNode) {
+	tree.AllorderTraverseVisit(root, nonFunc, printnode, nonFunc)
+}
+
+// nonFunc does nothing, used in preorder and postorder visits
+func nonFunc(node tree.Node) {
+}
+
+func printnode(node tree.Node) {
+	if node.IsNil() {
+		return
+	}
+	fmt.Printf("%s, ", node)
+}
