4adeb7696eae6153bbb38c1b4b0044d89df51456
Bruce Ediger <bediger8@gmail.com>
2022-02-26 12:55:40 -0700

    Different recursion

---
+++ list_from_tree4.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"binary_tree/tree"
+	"fmt"
+	"os"
+)
+
+/*
+LC 114 [Medium] Flatten Binary Tree to Linked List
+
+Question: Given a binary tree, flatten it to a linked list in-place.
+
+For example, given the following tree:
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+
+The flattened tree should look like:
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+*/
+
+func main() {
+
+	root := tree.CreateNumericFromString(os.Args[1])
+
+	flatten(root)
+
+	for n := root; n != nil; n = n.Right {
+		fmt.Printf("%d -> ", n.Data)
+	}
+	fmt.Println()
+}
+
+func flatten(node *tree.NumericNode) {
+	if node == nil || (node.Left == nil && node.Right == nil) {
+		return
+	}
+
+	if node.Left != nil {
+		flatten(node.Left)
+
+		tmp := node.Right
+		node.Right = node.Left
+		node.Left = nil
+
+		var t *tree.NumericNode
+		for t = node.Right; t.Right != nil; t = t.Right {
+		}
+
+		t.Right = tmp
+	}
+
+	flatten(node.Right)
+}
