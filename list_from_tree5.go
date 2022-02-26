7dbb318b4c51931baa84ed39cf163a3594b3e649
Bruce Ediger <bediger8@gmail.com>
2022-02-26 13:02:09 -0700

    completely iterative tree flattening

---
+++ list_from_tree5.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"binary_tree/tree"
+	"fmt"
+	"os"
+)
+
+/*
+LC 114 [Medium] Flatten Binary Tree to Linked List
+
+Question: Given a binary tree, flatten it to a linked list in-place.
+
+For example, given the following tree:
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+
+The flattened tree should look like:
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+*/
+
+func main() {
+
+	root := tree.CreateNumericFromString(os.Args[1])
+
+	flatten(root)
+
+	for n := root; n != nil; n = n.Right {
+		fmt.Printf("%d -> ", n.Data)
+	}
+	fmt.Println()
+}
+
+func flatten(node *tree.NumericNode) {
+	curr := node
+	for curr != nil {
+		if curr.Left != nil {
+			prev := curr.Left
+			for prev.Right != nil {
+				prev = prev.Right
+			}
+			prev.Right = curr.Right
+			curr.Right = curr.Left
+			curr.Left = nil
+		}
+		curr = curr.Right
+	}
+}
