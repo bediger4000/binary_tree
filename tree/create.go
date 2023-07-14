6169ba71f2e3a8d2cad99a17e6261778c82c9783
Bruce Ediger <bediger8@gmail.com>
2023-07-13 22:47:48 -0600

    Rewrite serializer/deserializer
    Deserializer can now be understood a little easier.
    
    Serializer makes more human-readable output

--- tree/create.go
+++ tree/create.go
@@ -5,7 +5,7 @@ import (
 	"io"
 	"os"
 	"strconv"
-	"unicode"
+	"strings"
 )
 
 // CreateNumeric converts a []string to a binary search tree
@@ -79,15 +79,26 @@ func Print(node Node) {
 // CreateNumericFromString can turn into a tree.
 func Printf(out io.Writer, node Node) {
 	if node.IsNil() {
-		fmt.Fprintf(out, "()")
 		return
 	}
-	fmt.Fprintf(out, "(%s", node) // relies on node fitting fmt.Stringer
-	if !node.LeftChild().IsNil() || !node.RightChild().IsNil() {
-		Printf(out, node.LeftChild())
-		Printf(out, node.RightChild())
+	leaf := node.LeftChild().IsNil() && node.RightChild().IsNil()
+	if !leaf {
+		fmt.Fprint(out, "(")
 	}
-	fmt.Fprintf(out, ")")
+	fmt.Fprintf(out, "%s", node)
+	if !node.LeftChild().IsNil() {
+		fmt.Fprint(out, " ")
+	} else if !node.RightChild().IsNil() {
+		fmt.Fprint(out, " ()")
+	}
+	Printf(out, node.LeftChild())
+	if !node.RightChild().IsNil() {
+		fmt.Fprint(out, " ")
+	}
+	Printf(out, node.RightChild())
+	if !leaf {
+		fmt.Fprint(out, ")")
+	}
 }
 
 // GeneralCreateFromString uses a func argument to create a tree
@@ -96,112 +107,63 @@ func Printf(out io.Writer, node Node) {
 // sets up to call genericTreeFromString with details that the caller
 // shouldn't have to know.
 func GeneralCreateFromString(stringrep string, nc NodeCreatorFn) Node {
-	runes := []rune(stringrep)
-	root, _ := genericTreeFromString(runes, 0, len(runes), nc)
+	tokens := tokenize(stringrep)
+	root, _ := genericTreeFromString(tokens, 0, nc)
 	return root
 }
 
-func genericTreeFromString(runes []rune, offset, end int, nodeCreator NodeCreatorFn) (Node, int) {
-	xoffset := eatWhiteSpace(runes, offset, end)
-	if runes[xoffset] != '(' {
-		fmt.Fprintf(os.Stderr, "no leading ( on %q at offset %d\n", string(runes), xoffset)
-	}
-	xend := findRightParen(runes, xoffset)
-	xoffset++ // skip '('
-
-	// eat whitespace after '(' but before string or number or whatever
-	xoffset = eatWhiteSpace(runes, xoffset, xend)
-
-	str, xoffset := readStuff(runes, xoffset, xend)
-
-	var node Node
-	if len(str) > 0 {
-		node = nodeCreator(str)
-	} else {
-		xoffset = eatWhiteSpace(runes, xoffset, xend)
-		xoffset++ // eat ')'
-		return nil, xoffset
+func genericTreeFromString(tokens []string, index int, nc NodeCreatorFn) (Node, int) {
+	l := len(tokens)
+	if index >= l || l == 0 {
+		return nil, index
 	}
 
-	// eat whitespace after string or number or whatever, before '(' or ')'
-	xoffset = eatWhiteSpace(runes, xoffset, xend)
+	var n Node
+	looping := true
+	setLeft := false
 
-	if runes[xoffset] == ')' {
-		xoffset++
-		return node, xoffset
-	}
-
-	leftChild, xoffset := genericTreeFromString(runes, xoffset, xend, nodeCreator)
-	node.SetLeftChild(leftChild)
-
-	if runes[xoffset] == ')' {
-		xoffset++
-		return node, xoffset
-	}
-
-	rightChild, xoffset := genericTreeFromString(runes, xoffset, xend, nodeCreator)
-	node.SetRightChild(rightChild)
-
-	xoffset = eatWhiteSpace(runes, xoffset, xend)
-	xoffset++ // eat trailing ')'
-
-	return node, xoffset
-}
-
-// readStuff returns a non-whitespace, non-parentheses string
-// from runes []rune, and an index of where that string ends.
-func readStuff(runes []rune, offset int, end int) (string, int) {
-	var valueRunes []rune
-	for {
-		if runes[offset] == '(' {
-			break
-		}
-		if runes[offset] == ')' {
-			break
-		}
-		if offset == end {
-			break
-		}
-		if unicode.IsSpace(runes[offset]) {
-			break
-		}
-		valueRunes = append(valueRunes, runes[offset])
-		offset++
-	}
-
-	return string(valueRunes), offset
-}
-
-// eatWhiteSpace starts at index offset in runes,
-// and returns the index of the next non-whitespace rune.
-func eatWhiteSpace(runes []rune, offset int, end int) int {
-	for unicode.IsSpace(runes[offset]) {
-		offset++
-		if offset == end {
-			break
-		}
-	}
-	return offset
-}
-
-// findRightParen takes an array of runes, where '(' is at
-// index 0, and a matching ')' is at some greater index.
-// Returns that greater index
-func findRightParen(r []rune, offset int) int {
-	stack := make([]rune, 1)
-	stack[0] = r[offset]
-	end := offset
-
-	for idx := offset + 1; len(stack) > 0; idx++ {
-		switch r[idx] {
-		case '(':
-			stack = append(stack, '(')
-		case ')':
-			if stack[len(stack)-1] == '(' {
-				stack = stack[0 : len(stack)-1]
+	for looping && index < l {
+		switch tokens[index] {
+		case "(":
+			index++
+			child, nextindex := genericTreeFromString(tokens, index, nc)
+			index = nextindex
+			if n == nil {
+				n = child
+			} else if setLeft {
+				n.SetRightChild(child)
+			} else {
+				n.SetLeftChild(child)
+				setLeft = true
 			}
+		case ")":
+			looping = false
+			index++
+		default:
+			if n == nil {
+				n = nc(tokens[index])
+			} else {
+				// else, naked string child
+				child := nc(tokens[index])
+				if setLeft {
+					n.SetRightChild(child)
+				} else {
+					setLeft = true
+					n.SetLeftChild(child)
+				}
+			}
+			index++
 		}
-		end++
 	}
-	return end + 1
+	return n, index
+}
+
+func tokenize(str string) []string {
+	var tokens []string
+	for _, token := range strings.Split(strings.ReplaceAll(strings.ReplaceAll(str, "(", " ( "), ")", " ) "), " ") {
+		if token != "" {
+			tokens = append(tokens, token)
+		}
+	}
+	return tokens
 }
