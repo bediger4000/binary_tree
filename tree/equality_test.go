e848c93074fd60bd2106c57a16cce4fd2a1a931c
Bruce Ediger <bediger8@gmail.com>
2025-10-17 21:07:17 -0600

    accomodate 2-value error return from CreateFromString

--- tree/equality_test.go
+++ tree/equality_test.go
@@ -6,13 +6,28 @@ func TestEquals(t *testing.T) {
 	var nilNumeric *NumericNode
 	var nilString *StringNode
 	var randomTree *NumericNode = RandomValueTree(1000, 20, true)
-	var stringTree1 *StringNode = CreateFromString("(a(b(bl)(br))(c(cl)(cr)))")
-	var stringTree2 *StringNode = CreateFromString("(a(b(bl)(br))(c()(cr)))")
-	var stringTree3 *StringNode = CreateFromString("(a(b(bl)(br))(c(cl)()))")
-	var stringTree4 *StringNode = CreateFromString("(a(b(bl)())(c(cl)(cr)))")
-	var stringTree5 *StringNode = CreateFromString("(a(b()(br))(c(cl)(cr)))")
-	var stringTree6 *StringNode = CreateFromString("(a(b(bl)(br))(c(cx)(cr)))")
-	var stringTree7 *StringNode = CreateFromString("(a(b(bl)(br))(c(cl)(cx)))")
+	var stringTree1, stringTree2, stringTree3, stringTree4, stringTree5 *StringNode
+	var stringTree6, stringTree7, stringTree8a, stringTree8b *StringNode
+	var stringTree9a, stringTree9b, stringTree10a, stringTree10b *StringNode
+	var stringTree11a, stringTree11b, stringTree12, stringTree13, stringTree14 *StringNode
+	stringTree1, _ = CreateFromString("(a(b(bl)(br))(c(cl)(cr)))")
+	stringTree2, _ = CreateFromString("(a(b(bl)(br))(c()(cr)))")
+	stringTree3, _ = CreateFromString("(a(b(bl)(br))(c(cl)()))")
+	stringTree4, _ = CreateFromString("(a(b(bl)())(c(cl)(cr)))")
+	stringTree5, _ = CreateFromString("(a(b()(br))(c(cl)(cr)))")
+	stringTree6, _ = CreateFromString("(a(b(bl)(br))(c(cx)(cr)))")
+	stringTree7, _ = CreateFromString("(a(b(bl)(br))(c(cl)(cx)))")
+	stringTree8a, _ = CreateFromString("(a()())")
+	stringTree8b, _ = CreateFromString("(a()())")
+	stringTree9a, _ = CreateFromString("(a(b)())")
+	stringTree9b, _ = CreateFromString("(a(b)())")
+	stringTree10a, _ = CreateFromString("(a()(b))")
+	stringTree10b, _ = CreateFromString("(a()(b))")
+	stringTree11a, _ = CreateFromString("(a()(b))")
+	stringTree11b, _ = CreateFromString("(a(b)())")
+	stringTree12, _ = CreateFromString("(1()())")
+	stringTree13, _ = CreateFromString("(1()())")
+	stringTree14, _ = CreateFromString("(a()())")
 	type args struct {
 		t1 Node
 		t2 Node
@@ -59,37 +74,37 @@ func TestEquals(t *testing.T) {
 		},
 		{
 			name: "one element identical string trees",
-			args: args{t1: CreateFromString("(a()())"), t2: CreateFromString("(a()())")},
+			args: args{t1: stringTree8a, t2: stringTree8b},
 			want: true,
 		},
 		{
 			name: "two element identical string trees",
-			args: args{t1: CreateFromString("(a(b)())"), t2: CreateFromString("(a(b)())")},
+			args: args{t1: stringTree9a, t2: stringTree9b},
 			want: true,
 		},
 		{
 			name: "two element identical string trees",
-			args: args{t1: CreateFromString("(a()(b))"), t2: CreateFromString("(a()(b))")},
+			args: args{t1: stringTree10a, t2: stringTree10b},
 			want: true,
 		},
 		{
 			name: "two element different string trees",
-			args: args{t1: CreateFromString("(a()(b))"), t2: CreateFromString("(a(b)())")},
+			args: args{t1: stringTree11a, t2: stringTree11b},
 			want: false,
 		},
 		{
 			name: "one element different type trees A",
-			args: args{t1: CreateFromString("(1()())"), t2: CreateNumeric([]string{"1"})},
+			args: args{t1: stringTree12, t2: CreateNumeric([]string{"1"})},
 			want: false,
 		},
 		{
 			name: "one element different type trees B",
-			args: args{t1: CreateNumeric([]string{"1"}), t2: CreateFromString("(1()())")},
+			args: args{t1: CreateNumeric([]string{"1"}), t2: stringTree13},
 			want: false,
 		},
 		{
 			name: "one string tree, one nil tree",
-			args: args{t1: CreateFromString("(a()())"), t2: nilString},
+			args: args{t1: stringTree14, t2: nilString},
 			want: false,
 		},
 		{
